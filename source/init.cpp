c5e0fb10cbeaee0a4b5e558fbb1efbd0f99f417e
Super Hackio <44330283+SuperHackio@users.noreply.github.com>
2024-04-10 13:33:48 -0700

    Add Init.cpp

---
+++ source/init.cpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "revolution.h"
+#include "kamek/hooks.h"
+
+typedef void (*Func)();
+extern Func __ctor_loc;
+extern Func __ctor_end;
+
+namespace {
+    // ----------------------------------------------------------------------------------------------------------------
+    // Calls all static initializers after GameSystem::initAfterStationedResourceLoaded((void)). This is necessary to initialize static variables and 
+    // constants in the BSS segment. Custom Nerve instances will be stored there, for example.
+
+    void init() {
+        for (Func* f = &__ctor_loc; f < &__ctor_end; f++) {
+            (*f)();
+        }
+    }
+
+#if defined(TWN) || defined(KOR)
+    kmBranch(0x804B6A64, init);
+#else
+    kmBranch(0x804B69F4, init);
+#endif
+}
